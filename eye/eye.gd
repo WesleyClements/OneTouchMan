dbf9096851c0a7e66de9afe16dde31777dbf8c91
Sean Maltby <seanmaltby@gmail.com>
2019-08-02 23:13:46 -0700

    Fix eye scaling

--- eye/eye.gd
+++ eye/eye.gd
@@ -1,17 +1,14 @@
 extends KinematicBody2D
 
-onready var collisionRight = $collisionRight
-
 var direction := 1
 
 func _ready():
 	pass
 	
-func _physics_process(delta):
-	if collisionRight.is_colliding():
+func _process(delta):
+	if $collisionRight.is_colliding():
 		direction = -1
 	
-	move_and_slide(Vector2(50 * direction, 0))
+	move_and_slide(Vector2(60 * direction, 0))
 	
-	scale = Vector2(direction, 1)
-	
+	$sprite.scale.x = direction
